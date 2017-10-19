7dc2566fa047d3d3f99bac3f99dc38af4cf0de53
Oleg Podolyan <olegvanich@gmail.com>
2017-10-19 09:15:59 +0300

    WebConfig is updated (resolvers)

--- src/main/java/oleg/podolyan/svatoapiboot/config/WebConfig.java
+++ src/main/java/oleg/podolyan/svatoapiboot/config/WebConfig.java
@@ -2,14 +2,40 @@ package oleg.podolyan.svatoapiboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter{
 
+    @Bean
+    public ViewResolver viewResolver(){
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        return resolver;
+    }
+
+    private ITemplateEngine templateEngine() {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(templateResolver());
+        return springTemplateEngine;
+    }
+
+    private ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(false);       // in develop mode
+        return resolver;
+    }
 
     @Bean
     public Java8TimeDialect java8TimeDialect() {
